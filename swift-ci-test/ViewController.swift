62c6a2f40e02018634c42d9dfa114b5a4bc80c41
Thomas Degry <degry.thomas@gmail.com>
2016-02-01 14:54:50 -0500

    First commit

---
+++ swift-ci-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-ci-test
+//
+//  Created by LOANER on 2/1/16.
+//  Copyright © 2016 Thomas Degry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
